cbe2a027ebb0172f89e1c6be757c9b3b6bece98a
simranms <123356413+simranms@users.noreply.github.com>
2023-04-12 18:34:36 +0530

    MUL_UNIT

---
+++ multi_add_top.cpp
@@ -0,0 +1,29 @@
+#include <systemc.h>
+
+SC_MODULE(top) {
+    sc_clock clock;
+    sc_signal<bool> reset;
+    sc_vector<sc_signal<int>> inputs;
+    sc_signal<int> output;
+    multi_addition adder;
+
+    SC_CTOR(top) : 
+        clock("clock", 10, SC_NS), 
+        adder("adder", 3)
+    {
+        reset.write(false);
+        adder.clock(clock);
+        adder.reset(reset);
+        for (int i = 0; i < 3; i++) {
+            inputs[i].write(i);
+            adder.inputs[i](inputs[i]);
+        }
+        adder.output(output);
+    }
+};
+
+int sc_main(int argc, char* argv[]) {
+    top my_top("my_top");
+    sc_start(100, SC_NS);
+    return 0;
+}
